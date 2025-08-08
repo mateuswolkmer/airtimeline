ab81dffa31ce20b2e3ac337995e689d9c4492848
Mateus Wolkmer <mateuswsouza@gmail.com>
2025-08-08 15:11:56 -0300

    chore: setup Tailwind and Motion

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import timelineItems from "./timelineItems.js";
+import { motion } from "motion/react";
 
 function App() {
   return (
     <div>
-      <h2>Good luck with your assignment! {"\u2728"}</h2>
-      <h3>{timelineItems.length} timeline items to render</h3>
+      <motion.h2 drag className="text-2xl font-bold">
+        Good luck with your assignment! {"\u2728"}
+      </motion.h2>
+      <motion.h3>{timelineItems.length} timeline items to render</motion.h3>
     </div>
   );
 }
